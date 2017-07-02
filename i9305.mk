0c6f05b10d1991f021bae7dc77bb80386bd303f7
rodman01 <emaol4joern@arcor.de>
2017-07-02 11:33:00 +0200

    update path to vendor/samsung to pans vendor blobs, taken from and credits to @p.a.n from xda: https://forum.xda-developers.com/galaxy-s3/development-i9305/rom-unofficial-lineageos-14-1-t3627646
    Change-Id: Ie26b8fee1b2c343bd933b90d048838ba37bae379

--- i9305.mk
+++ i9305.mk
@@ -98,9 +98,9 @@ PRODUCT_COPY_FILES += \
 # Configure dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
-# Include common makefile first
+# Include device blobs first
+$(call inherit-product, vendor/samsung/i9305/i9305-vendor.mk)
+
+# Include common makefile
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/smdk4412-qcom-common/common.mk)
-
-# Include device blobs
-$(call inherit-product, vendor/samsung/i9305-propril/i9305-vendor.mk)
